169aeccf4f8cc499bebde270b71395ae55b6d221
Florian <skatepainter@yahoo.de>
2016-07-08 19:54:16 +0200

    Implemented country model and hibernate SessionFactory.

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,2 +1,44 @@
+import com.floriantoenjes.analyzer.model.Country;
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.service.ServiceRegistry;
+
+import java.util.List;
+
 public class Application {
+    private static final SessionFactory sessionFactory = buildSessionFactory();
+
+    private static SessionFactory buildSessionFactory() {
+        final ServiceRegistry registry = new StandardServiceRegistryBuilder().configure().build();
+        return new MetadataSources(registry).buildMetadata().buildSessionFactory();
+    }
+
+    public static void main(String[] args) {
+        Country country = new Country("deu", "Deutschland");
+
+        Session session = sessionFactory.openSession();
+
+        session.beginTransaction();
+
+        session.save(country);
+
+        session.getTransaction().commit();
+
+        session.close();
+
+
+        session = sessionFactory.openSession();
+
+        Criteria criteria = session.createCriteria(Country.class);
+
+        List<Country> countries = criteria.list();
+
+        session.close();
+
+        countries.forEach(System.out::println);
+    }
+
 }
--- src/main/java/com/floriantoenjes/analyzer/model/Country.java
+++ src/main/java/com/floriantoenjes/analyzer/model/Country.java
@@ -1,8 +1,68 @@
 package com.floriantoenjes.analyzer.model;
 
+import javax.persistence.*;
+
+@Entity
 public class Country {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private String code;
+
+    @Column
     private String name;
+
+    @Column
     private double adultLiteracyRate;
+
+    @Column
     private double internetUsers;
+
+    public Country() {}
+
+    public Country(String code, String name) {
+        this.code = code;
+        this.name = name;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getAdultLiteracyRate() {
+        return adultLiteracyRate;
+    }
+
+    public void setAdultLiteracyRate(double adultLiteracyRate) {
+        this.adultLiteracyRate = adultLiteracyRate;
+    }
+
+    public double getInternetUsers() {
+        return internetUsers;
+    }
+
+    public void setInternetUsers(double internetUsers) {
+        this.internetUsers = internetUsers;
+    }
+
+    @Override
+    public String toString() {
+        return "Country{" +
+                "code='" + code + '\'' +
+                ", name='" + name + '\'' +
+                ", adultLiteracyRate=" + adultLiteracyRate +
+                ", internetUsers=" + internetUsers +
+                '}';
+    }
 }
